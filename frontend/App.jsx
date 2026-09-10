31e26da72095bd2b38b371d3aceb212f40556972
LovKimtech <Lovkimtech@gmail.com>
2026-09-10 14:41:10 +0700

    Fix SignIn component reference

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -75,7 +75,7 @@ export default function App() {
           path="/signin"
           element={
             <AuthRedirect>
-              <Signin />
+              <SignIn />
             </AuthRedirect>
           }
         />
